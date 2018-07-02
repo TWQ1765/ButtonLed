654ff8586c3e70a199020fa2583d57074aed3468
TWQ1765 <www.taaadaaa@qq.com>
2018-07-02 16:14:43 +0800

    logit dotapTurnOntapTurnOffLed function fixed and completed.

--- src/ledController.c
+++ src/ledController.c
@@ -3,6 +3,9 @@
 #include "Button.h"
 #include <stdio.h>
 
+#define SITUATION_LED_OFF 0;
+#define SITUATION_LED_ON 1;
+
 void turnOnLedIfButtonIsPressed(void){ //not put while loop here
 	
 	if(getButtonState())
@@ -94,30 +97,47 @@ void dotapTurnOntapTurnOffLed(LedButtonInfo *state){
 	//turnLed(state->currentLedState);
 	//*/
  int currentButton = getButtonState();
- 
+ int situation;
  switch (state->currentLedState){
 	 
 	 case LED_OFF :
-	 if (state->previousButtonState == currentButton){
+	 if ((state->previousButtonState == currentButton) && (currentButton == BUTTON_RELEASE)){
 		 state->currentLedState = LED_OFF;
 		 state->previousButtonState = currentButton;
 	 }
 	 else if(state->previousButtonState != currentButton){
 		 state->currentLedState = LED_ON;
 		 state->previousButtonState = currentButton;
+		 situation = SITUATION_LED_OFF;
 		 turnLed(LED_ON);
 	 }
 	 else{state->currentLedState = LED_OFF;}
 	 break;
+	 
 	 case LED_ON :
-	  if (state->previousButtonState != currentButton){
+	  if ((state->previousButtonState != currentButton) && (currentButton == BUTTON_PRESSED)){
+		 state->currentLedState = LED_ON;
+		 state->previousButtonState = currentButton;
+		 situation = SITUATION_LED_ON;		 
+	 }
+	 else if((state->previousButtonState == currentButton) && (currentButton == BUTTON_RELEASE)){
 		 state->currentLedState = LED_ON;
 		 state->previousButtonState = currentButton;
 	 }
-	 else if(state->previousButtonState == currentButton){
-		 state->currentLedState = LED_OFF;
-		 state->previousButtonState = currentButton;
-		 turnLed(LED_OFF);
+	 else if ((state->previousButtonState != currentButton) && (currentButton == BUTTON_RELEASE)){
+		 switch (situation){
+			 case 0:
+					state->currentLedState = LED_ON;
+					state->previousButtonState = currentButton;
+					situation = SITUATION_LED_ON;
+			 break;
+			 case 1:
+					state->currentLedState = LED_OFF;
+					state->previousButtonState = currentButton;
+					turnLed(LED_OFF);
+			 break;
+			 default:{state->currentLedState = LED_OFF;}
+		 }
 	 }
 	 else{state->currentLedState = LED_OFF;}
 	 
